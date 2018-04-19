afa6042cec2b0b0908727b4c537382ca91f5098b
Marco Marchitelli <marchitelli97@libero.it>
2018-04-19 15:20:44 +0200

    Ecco qua!!

---
+++ Switching Strikes/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    [Header("Color")]
+    public ColorType color;
+
+    float speed;
+
+    private void Start()
+    {
+        speed = SpawnManager.instance.enemiesSpeed;
+        StartCoroutine(AttackAnimation(new Vector3(0, transform.position.y, 0), speed));
+    }
+
+    IEnumerator AttackAnimation(Vector3 destination, float speed)
+    {
+        while (transform.position != destination)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void Die()
+    {
+        if(SpawnManager.instance.enemiesAlive > 0)
+            SpawnManager.instance.enemiesAlive--;
+        Destroy(gameObject);
+    }
+
+}
